5f0e2d7ea269abe0505b8af4f5f24a51b2de4d9c
MarcoLupo94 <lupo.marco94@gmail.com>
2022-12-28 15:37:21 +0000

    Added transparency and animation to nav-bar

--- apps/caroline-lerus/app/nav-bar/nav-bar.tsx
+++ apps/caroline-lerus/app/nav-bar/nav-bar.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react';
 import styles from './nav-bar.module.css';
 
 /* eslint-disable-next-line */
 export interface NavBarProps {}
 
 export function NavBar(props: NavBarProps) {
+  const [color, setColor] = useState('transparent');
+  useEffect(() => {
+    const handleScroll = (event) => {
+      if (window.scrollY > 0) setColor('white');
+      else setColor('transparent');
+    };
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className={styles['container']}>
+    <nav
+      className={styles['container']}
+      style={{ backgroundColor: `${color}` }}
+    >
       <h3>CLR 👩🏾‍🍳</h3>
       <ul className={styles['links']}>
         {' '}
