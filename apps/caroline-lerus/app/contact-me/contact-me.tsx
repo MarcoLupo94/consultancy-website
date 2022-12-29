4fed0905a847bd3e5d853e7dcb06967ad8b1f950
MarcoLupo94 <lupo.marco94@gmail.com>
2022-12-29 20:13:22 +0000

    Added event handler to form

--- apps/caroline-lerus/app/contact-me/contact-me.tsx
+++ apps/caroline-lerus/app/contact-me/contact-me.tsx
@@ -6,31 +6,54 @@ import styles from './contact-me.module.css';
 export interface ContactMeProps {}
 
 export function ContactMe(props: ContactMeProps) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const infos = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    console.log(infos);
+  };
   return (
     <section
       className={styles['container']}
       id="contact-me"
       style={{ height: '100%' }}
     >
-      <form className={styles['form']}>
+      <form className={styles['form']} onSubmit={handleSubmit}>
         <h3>Contact Us</h3>
         <TextInput
           rightSection={<FiAlertTriangle size={16} />}
           label="Your name"
           placeholder="Your name"
+          name="name"
+          type="text"
+          required
         />
         <TextInput
           rightSection={<FiAlertTriangle size={16} />}
           label="Your email"
           placeholder="Your email"
+          name="email"
+          required
+          type="email"
         />
         <TextInput
           rightSection={<FiAlertTriangle size={16} />}
           label="Subject"
           placeholder="Subject"
+          name="subject"
+          required
+          type="text"
         />
-        <Textarea label="Message(optional)" placeholder="Text..." />
-        <Button>Submit</Button>
+        <Textarea
+          label="Message(optional)"
+          placeholder="Text..."
+          name="message"
+        />
+        <Button type="submit">Submit</Button>
       </form>
     </section>
   );
